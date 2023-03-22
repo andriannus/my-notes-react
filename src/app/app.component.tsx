2b9a2887fac63a7a0dcbb0893cbc4cc893ae21f3
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-03-22 21:46:53 +0700

    feat: install and setup react-query

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC, Suspense } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -5,19 +6,29 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "@/app/app.routes";
 import { AuthProvider, NoteProvider } from "@/contexts";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const App: FC<Record<string, unknown>> = () => {
   return (
-    <Router>
-      <Suspense>
-        <HelmetProvider>
-          <AuthProvider>
-            <NoteProvider>
-              <AppRoutes />
-            </NoteProvider>
-          </AuthProvider>
-        </HelmetProvider>
-      </Suspense>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Suspense>
+          <HelmetProvider>
+            <AuthProvider>
+              <NoteProvider>
+                <AppRoutes />
+              </NoteProvider>
+            </AuthProvider>
+          </HelmetProvider>
+        </Suspense>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
