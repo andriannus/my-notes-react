975640799dc4626414bc98933d4d604cc4ad8d9a
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-03-22 17:21:01 +0700

    feat: forward ref `text-field` component

--- src/components/text-field/text-field.component.tsx
+++ src/components/text-field/text-field.component.tsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import {
   ChangeEvent,
   ChangeEventHandler,
-  FC,
+  forwardRef,
   HTMLInputTypeAttribute,
   useCallback,
   useMemo,
@@ -25,59 +25,65 @@ interface TextFieldProps {
   value: string;
 }
 
-const TextField: FC<Partial<TextFieldProps>> = ({
-  autoCapitalize = "",
-  autoComplete = "",
-  className = "",
-  counter = 0,
-  disabled = false,
-  id = "",
-  name = "",
-  onChange = () => {},
-  placeholder = "",
-  readOnly = false,
-  type = "text",
-  value = "",
-}) => {
-  const valueLength = useMemo(() => {
-    return value.length;
-  }, [value]);
-
-  const handleOnChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      onChange(event);
+const TextField = forwardRef<HTMLInputElement, Partial<TextFieldProps>>(
+  (
+    {
+      autoCapitalize = "",
+      autoComplete = "",
+      className = "",
+      counter = 0,
+      disabled = false,
+      id = "",
+      name = "",
+      onChange = () => {},
+      placeholder = "",
+      readOnly = false,
+      type = "text",
+      value = "",
     },
-    [onChange]
-  );
+    ref
+  ) => {
+    const valueLength = useMemo(() => {
+      return value.length;
+    }, [value]);
 
-  return (
-    <div className={className}>
-      <input
-        {...(!!counter && { maxLength: counter })}
-        id={id}
-        autoCapitalize={autoCapitalize}
-        autoComplete={autoComplete}
-        className="TextField-input"
-        disabled={disabled}
-        name={name}
-        placeholder={placeholder}
-        readOnly={readOnly}
-        title={placeholder}
-        type={type}
-        value={value}
-        onChange={handleOnChange}
-      />
+    const handleOnChange = useCallback(
+      (event: ChangeEvent<HTMLInputElement>) => {
+        onChange(event);
+      },
+      [onChange]
+    );
 
-      {!!counter && (
-        <div className="TextField-counter">
-          <span>
-            {valueLength} / {counter}
-          </span>
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className={className}>
+        <input
+          ref={ref}
+          id={id}
+          {...(!!counter && { maxLength: counter })}
+          autoCapitalize={autoCapitalize}
+          autoComplete={autoComplete}
+          className="TextField-input"
+          disabled={disabled}
+          name={name}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          title={placeholder}
+          type={type}
+          value={value}
+          onChange={handleOnChange}
+        />
+
+        {!!counter && (
+          <div className="TextField-counter">
+            <span>
+              {valueLength} / {counter}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 TextField.propTypes = {
   autoCapitalize: PropTypes.string,
