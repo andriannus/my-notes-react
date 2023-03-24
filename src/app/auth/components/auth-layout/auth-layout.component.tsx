e00477cc2e64ae9ceadf9604908537b1904c1d53
Andriannus Parasian <andriannus.parasian@efishery.com>
2023-03-24 10:34:56 +0700

    refactor: add missing propTypes

--- src/app/auth/components/auth-layout/auth-layout.component.tsx
+++ src/app/auth/components/auth-layout/auth-layout.component.tsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { FC, PropsWithChildren } from "react";
 
 import { AuthLayoutBody } from "./auth-layout-body";
@@ -7,6 +8,10 @@ import "./auth-layout.component.scss";
 
 const AuthLayout: FC<PropsWithChildren> = ({ children }) => {
   return <main className="AuthLayout">{children}</main>;
+};
+
+AuthLayout.propTypes = {
+  children: PropTypes.node,
 };
 
 export default Object.assign(AuthLayout, {
